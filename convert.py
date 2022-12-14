532395d823f971e97625d7aa56f0d1f624e5cd1f
Will <wyfcui@gmail.com>
2022-12-14 01:48:52 +0000

    convert file bugs

--- convert.py
+++ convert.py
@@ -1,10 +1,13 @@
 import os
-import tqdm
+import subprocess
+
+from tqdm import tqdm
 
 data_dir = './data'
 stages = ['test', 'train']
 
 for stage in stages:
-    for i, file in tqdm(enumerate(os.listdir("{}/{}".format(data_dir, stage)))):
-        os.system(
-            "ffmpeg -i test/{} -vn -acodec pcm_s16le -map 0:a:1 -ar 44100 {}/{}.wav".format(file, stage+"_mod", i))
+    for i, file in tqdm(enumerate(sorted(os.listdir("{}/{}".format(data_dir, stage))))):
+        for track in range(5):
+            command = ["ffmpeg", "-i", "{}/{}/{}".format(data_dir, stage, file), "-vn", "-acodec", "pcm_s16le", "-map", "0:a:{}".format(track), "-ar", "44100", "{}/{}/{}_{}.wav".format(data_dir, stage+"_mod", i, track)]
+            subprocess.run(command)
